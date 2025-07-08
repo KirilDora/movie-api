4d8fe74b6eb930dc388d8b58f6f4ebd76aea71eb
KirilDora <kirildora02@gmail.com>
2025-07-08 12:59:56 +0300

    created User model for authentication

--- src/models/index.js
+++ src/models/index.js
@@ -2,5 +2,6 @@ const sequelize = require('../config/database');
 const Movie = require('./movie.model');
 const Actor = require('./actor.model');
 const MovieActor = require('./movieActor.model');
+const User = require('./user.model');
 
-module.exports = { sequelize, Movie, Actor, MovieActor };
+module.exports = { sequelize, Movie, Actor, MovieActor, User };
--- src/routes/movies.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const movieController = require('../controllers/movies.controller');
-
-router.get('/', movieController.getAllMovies);
-router.post('/', movieController.addMovie);
-
-module.exports = router;
