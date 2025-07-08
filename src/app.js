cc2c11e21699921253cc543893f630e04bb84a52
KirilDora <kirildora02@gmail.com>
2025-07-08 12:46:11 +0300

    added version to routes

--- src/app.js
+++ src/app.js
@@ -2,14 +2,16 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const { sequelize } = require('./models');
-const movieRoutes = require('./routes/movies.routes');
+const movieRoutes = require('./routes/api/v1/movies.routes');
+const errorHandler = require('./utils/errorHandler');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(errorHandler);
 
-app.use('/movies', movieRoutes);
+app.use('/api/v1/movies', movieRoutes);
 
 const PORT = process.env.APP_PORT || 3000;
 
