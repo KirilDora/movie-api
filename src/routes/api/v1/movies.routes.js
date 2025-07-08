9c5beaafbfa6474eb317972d07d43896084079df
KirilDora <kirildora02@gmail.com>
2025-07-08 14:18:43 +0300

    added auth before executing api

--- src/routes/api/v1/movies.routes.js
+++ src/routes/api/v1/movies.routes.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../../middleware/auth.middleware');
 const movieController = require('../../../controllers/movies.controller');
 
-router.get('/', movieController.getAllMovies);
-router.post('/', movieController.addMovie);
+router.get('/', auth, movieController.getAllMovies);
+router.post('/', auth, movieController.addMovie);
 
 module.exports = router;
